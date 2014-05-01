1fc376a4b815f055d0cbd795e2f9a35665de4269
Qwlouse <qwlouse@gmail.com>
2014-05-01 11:53:37 +0200

    WIP: migrating to py.test

--- tests/test_arg_parser.py
+++ tests/test_arg_parser.py
@@ -3,31 +3,24 @@
 
 from __future__ import division, print_function, unicode_literals
 
-import unittest
 from sacred.arg_parser import parse_mongo_db_arg
 
 
-class ArgParserTests(unittest.TestCase):
-    def test_parse_mongo_db_arg(self):
-        self.assertTupleEqual(parse_mongo_db_arg('foo'),
-                              ('localhost:27017', 'foo'))
+def test_parse_mongo_db_arg():
+    assert parse_mongo_db_arg('foo') == ('localhost:27017', 'foo')
 
-    def test_parse_mongo_db_arg_hostname(self):
-        self.assertTupleEqual(parse_mongo_db_arg('localhost:28017'),
-                              ('localhost:28017', 'sperment'))
 
-        self.assertTupleEqual(parse_mongo_db_arg('www.mymongo.db:28017'),
-                              ('www.mymongo.db:28017', 'sperment'))
+def test_parse_mongo_db_arg_hostname():
+    assert parse_mongo_db_arg('localhost:28017') == ('localhost:28017', 'sperment')
 
-        self.assertTupleEqual(parse_mongo_db_arg('123.45.67.89:27017'),
-                              ('123.45.67.89:27017', 'sperment'))
+    assert parse_mongo_db_arg('www.mymongo.db:28017') == ('www.mymongo.db:28017', 'sperment')
 
-    def test_parse_mongo_db_arg_hostname_dbname(self):
-        self.assertTupleEqual(parse_mongo_db_arg('localhost:28017:foo'),
-                              ('localhost:28017', 'foo'))
+    assert parse_mongo_db_arg('123.45.67.89:27017') == ('123.45.67.89:27017', 'sperment')
 
-        self.assertTupleEqual(parse_mongo_db_arg('www.mymongo.db:28017:bar'),
-                              ('www.mymongo.db:28017', 'bar'))
 
-        self.assertTupleEqual(parse_mongo_db_arg('123.45.67.89:27017:baz'),
-                              ('123.45.67.89:27017', 'baz'))
+def test_parse_mongo_db_arg_hostname_dbname():
+    assert parse_mongo_db_arg('localhost:28017:foo') == ('localhost:28017', 'foo')
+
+    assert parse_mongo_db_arg('www.mymongo.db:28017:bar') == ('www.mymongo.db:28017', 'bar')
+
+    assert parse_mongo_db_arg('123.45.67.89:27017:baz') == ('123.45.67.89:27017', 'baz')
