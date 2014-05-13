2f8b2735131809f584b66673c5802a4b4d9213b9
Klaus Greff <klaus@idsia.ch>
2014-05-13 19:03:00 +0200

    changed #!/usr/bin/python to #!/usr/bin/env python

--- examples/pylstm_example.py
+++ examples/pylstm_example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding=utf-8
 """
 This is an example experiment of how you might use sperment together with
--- sacred/experiment.py
+++ sacred/experiment.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding=utf-8
 
 from __future__ import division, print_function, unicode_literals
