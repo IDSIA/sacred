46f837a7c4b12782485912c11915a0317cf69deb
Qwlouse <qwlouse@gmail.com>
2015-02-19 23:27:10 +0100

    refactored config_file handling and tested it

--- sacred/config_files.py
+++ sacred/config_files.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
+import json
+import pickle
 import os
 
 
-def json_loader(filename):
-    import json
-    with open(filename, 'r') as f:
-        return json.load(f)
+class Handler(object):
+    def __init__(self, load, dump, mode):
+        self.load = load
+        self.dump = dump
+        self.mode = mode
 
-
-def pickle_loader(filename):
-    import pickle
-    with open(filename, 'r') as f:
-        return pickle.load(f)
-
-
-def yaml_loader(filename):
-    try:
-        import yaml
-    except ImportError:
-        raise ImportError('Failed to import PyYAML.')
-
-    with open(filename, 'r') as f:
-        return yaml.load(f)
-
-
-LOADER_BY_EXT = {
-    'json': json_loader,
-    'pickle': pickle_loader,
-    'yaml': yaml_loader
+HANDLER_BY_EXT = {
+    '.json': Handler(json.load, json.dump, ''),
+    '.pickle': Handler(pickle.load, pickle.dump, 'b'),
 }
+
+
+try:
+    import yaml
+    HANDLER_BY_EXT['.yaml'] = Handler(yaml.load, yaml.dump, '')
+except ImportError:
+    pass
 
 
 def load_config_file(filename):
     _, extension = os.path.splitext(filename)
-    return LOADER_BY_EXT[extension](filename)
+    handler = HANDLER_BY_EXT[extension]
+    with open(filename, 'r' + handler.mode) as f:
+        return handler.load(f)
