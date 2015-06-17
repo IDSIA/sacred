9037b38cc1dbbd3804929bf22336b1bd5b2da2ea
Gerben <gerben@treora.com>
2015-06-17 17:18:13 +0300

    Support using @ex.capture on methods.
    When constructing arguments for a bound method invocation, the first
    argument of the signature ('self') should be ignored, as it is passed
    automatically.

--- sacred/config/captured_function.py
+++ sacred/config/captured_function.py
@@ -38,7 +38,9 @@ def captured_function(wrapped, instance, args, kwargs):
         options['_seed'] = get_seed(wrapped.rnd)
         options['_rnd'] = create_rnd(options['_seed'])
 
-    args, kwargs = wrapped.signature.construct_arguments(args, kwargs, options)
+    bound = (instance is not None)
+    args, kwargs = wrapped.signature.construct_arguments(args, kwargs, options,
+                                                         bound)
     wrapped.logger.debug("Started")
     start_time = time.time()
     # =================== run actual function =================================
