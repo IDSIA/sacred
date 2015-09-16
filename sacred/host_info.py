32aab37cfad1cbfe6f0a146e0dd4f099e1e12c17
Qwlouse <qwlouse@gmail.com>
2015-09-16 19:15:15 +0200

    minor refactorings and added some docs to host_info

--- sacred/host_info.py
+++ sacred/host_info.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""This module helps to collect information about the host of an experiment."""
 from __future__ import division, print_function, unicode_literals
 
 import multiprocessing
@@ -11,7 +12,25 @@ import subprocess
 __sacred__ = True  # marks files that should be filtered from stack traces
 
 
-def get_processor_name():
+def get_host_info():
+    """Collect some information about the machine this experiment runs on.
+
+    :return: A dictionary with information about the CPU, the OS and the
+             Python version of this machine.
+    :rtype: dict
+    """
+    return {
+        "cpu": _get_processor_name(),
+        "cpu_count": multiprocessing.cpu_count(),
+        "hostname": platform.node(),
+        "os": platform.system(),
+        "os_info": platform.platform(),
+        "python_version": platform.python_version(),
+        "python_compiler": platform.python_compiler()
+    }
+
+
+def _get_processor_name():
     if platform.system() == "Windows":
         return platform.processor().strip()
     elif platform.system() == "Darwin":
@@ -25,15 +44,3 @@ def get_processor_name():
         for line in all_info.split("\n"):
             if model_pattern.match(line):
                 return model_pattern.sub("", line, 1).strip()
-
-
-def get_host_info():
-    return {
-        "cpu": get_processor_name(),
-        "cpu_count": multiprocessing.cpu_count(),
-        "hostname": platform.node(),
-        "os": platform.system(),
-        "os_info": platform.platform(),
-        "python_version": platform.python_version(),
-        "python_compiler": platform.python_compiler()
-    }
