e2acfa35dd4205973fee29742bd6f77cd4247ca6
Qwlouse <qwlouse@gmail.com>
2016-01-13 19:54:05 +0100

    added a more helpful message when using -m/-mongo_db and the pymongo package is missing

--- sacred/observers/__init__.py
+++ sacred/observers/__init__.py
@@ -2,6 +2,8 @@
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
 
+from sacred.commandline_options import CommandLineOption
+
 from sacred.observers.base import RunObserver
 import sacred.optional as opt
 
@@ -9,5 +11,16 @@ if opt.has_pymongo:
     from sacred.observers.mongo import MongoObserver
 else:
     MongoObserver = opt.MissingDependencyMock('pymongo')
+
+    class MongoDbOption(CommandLineOption):
+        """To use the MongoObserver you need to install pymongo first."""
+
+        arg = 'DB'
+
+        @classmethod
+        def apply(cls, args, run):
+            raise ImportError('cannot use -m/--mongo_db flag: '
+                              'missing pymongo dependency')
+
 
 __all__ = ('RunObserver', 'MongoObserver')
