7ec332cdfe29d9e8e51142f22da02240e52c586f
Qwlouse <qwlouse@gmail.com>
2016-01-13 13:40:21 +0100

    windows build on python3.3 failed because of that line.
    Let's see if this helps...

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args.clear()
         self.test_suite = True
 
     def run_tests(self):
