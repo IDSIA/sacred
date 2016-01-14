5b2103d79cf69f0e715205a1e0038c67d58d8f72
Qwlouse <qwlouse@gmail.com>
2016-01-14 12:12:31 +0100

    fixing the RTD documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,12 @@
 import os
 import os.path
 import sys
-from sacred import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+from sacred import __version__
 
 # -- General configuration ------------------------------------------------
 
