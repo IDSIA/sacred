329a454b3a97fca0118fff3dd671885eb084b29d
Chris Musselle <chris.j.musselle@gmail.com>
2016-11-02 22:42:25 +0000

    Initial implimentation of tinydb observer pre test

---
+++ sacred/observers/tinydb.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import datetime as dt 
+import uuid
+
+from sacred.__about__ import __version__
+from sacred.observers import RunObserver
+from sacred.dependencies import get_digest
+import sacred.optional as opt
+
+from tinydb import TinyDB
+from tinydb.middlewares import Middleware
+from tinydb.storages import JSONStorage
+
+from hashfs import HashFS
+
+
+class TinyDbObserver(RunObserver):
+
+    VERSION = "TinyDbObserver-{}".format(__version__)
+
+    @staticmethod 
+    def create(path='.', name='observer_db', overwrite=None):
+
+        location = os.path.abspath(path)
+        root_dir = os.path.join(location, name)
+        if not os.path.exists(root_dir):
+            os.makedirs(root_dir)
+
+        db = TinyDB(os.path.join(root_dir, 'metadata.json'),
+                    storage=SerializeArrayDataFrameMiddleware(JSONStorage))
+        fs = HashFS(os.path.join(root_dir, 'hashfs'), depth=3, width=2, algorithm='md5')
+
+        return TinyDbObserver(db, fs, overwrite=overwrite)
+
+    def __init__(self, db, fs, overwrite=None):
+        self.db = db
+        self.runs = db.table('runs')
+        self.fs = fs
+        self.overwrite = overwrite
+        self.run_entry = {}
+        self.db_run_id = None
+
+    def save(self):
+        """ Insert or update the current entry """
+        if self.db_run_id:
+            self.runs.update(self.run_entry, eids=[self.db_run_id])
+        else:
+            db_run_id = self.runs.insert(self.run_entry)
+            self.db_run_id = db_run_id
+
+    def save_sources(self, ex_info):
+        
+        # base_dir = ex_info['base_dir']
+        source_info = []
+        for source_name, md5 in ex_info['sources']:
+        
+            file = self.fs.get(md5)
+            if file:
+                id_ = file.id
+            else:
+                # Substitute any HOME or Environment Vars to get absolute path
+                abs_path = os.path.expanduser(source_name)
+                abs_path = os.path.expandvars(source_name)
+                with open(abs_path, 'rb') as f:
+                    address = self.fs.put(f)
+                    id_ = address.id
+            source_info.append([source_name, id_])
+        return source_info
+
+    def queued_event(self, ex_info, command, queue_time, config, meta_info,
+                     _id):
+        raise NotImplementedError('queued_event method is not implimented for local TinyDbObserver.')
+
+    def started_event(self, ex_info, command, host_info, start_time, config,
+                      meta_info, _id):
+
+        self.run_entry = {
+            'experiment': dict(ex_info),
+            'format': self.VERSION,
+            'command': command,
+            'host': dict(host_info),
+            'start_time': start_time,
+            'config': config,
+            'meta': meta_info,
+            'status': 'RUNNING',
+            'resources': [],
+            'artifacts': [],
+            'captured_out': '',
+            'info': {},
+            'heartbeat': None
+        }
+
+        # set ID if not given
+        if _id is None:
+            _id = uuid.uuid4().hex
+
+        self.run_entry['_id'] = _id
+        
+        # save sources
+        self.run_entry['experiment']['sources'] = self.save_sources(ex_info)
+        self.save()
+        return self.run_entry['_id']
+
+    def heartbeat_event(self, info, cout_filename, beat_time):
+        self.run_entry['info'] = info
+        with open(cout_filename, 'r') as f:
+            self.run_entry['captured_out'] = f.read()
+        self.run_entry['heartbeat'] = beat_time
+        self.save()
+
+    def completed_event(self, stop_time, result):
+        self.run_entry['stop_time'] = stop_time
+        self.run_entry['result'] = result
+        self.run_entry['status'] = 'COMPLETED'
+        self.save()
+
+    def interrupted_event(self, interrupt_time, status):
+        self.run_entry['stop_time'] = interrupt_time
+        self.run_entry['status'] = status
+        self.save()
+
+    def failed_event(self, fail_time, fail_trace):
+        self.run_entry['stop_time'] = fail_time
+        self.run_entry['status'] = 'FAILED'
+        self.run_entry['fail_trace'] = fail_trace
+        self.save()
+
+    def resource_event(self, filename):
+
+        md5hash = get_digest(filename)
+        file_ = self.fs.get(md5hash)
+
+        if file_:
+            resource = (filename, md5hash)
+            if resource not in self.run_entry['resources']:
+                self.run_entry['resources'].append(resource)
+                self.save()
+        else: 
+            self.fs.put(filename)
+            self.run_entry['resources'].append((filename, md5hash))
+            self.save()
+
+    def artifact_event(self, filename):
+
+        md5hash = get_digest(filename)
+        file_ = self.fs.get(md5hash)
+
+        if file_:
+            artifact = (filename, md5hash)
+            if artifact not in self.run_entry['artifacts']:
+                self.run_entry['artifacts'].append(artifact)
+                self.save()
+        else: 
+            self.fs.put(filename)
+            self.run_entry['artifacts'].append((filename, md5hash))
+            self.save()
+
+    def __eq__(self, other):
+        if isinstance(other, TinyDbObserver):
+            return self.runs == other.runs
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class SerializeArrayDataFrameMiddleware(Middleware):
+
+    """ Custom Middleware to handle arrays and DataFrame serialisation """
+    
+    def __init__(self, storage_cls=TinyDB.DEFAULT_STORAGE):
+        # Any middleware *has* to call the super constructor
+        # with storage_cls
+        super(SerializeArrayDataFrameMiddleware, self).__init__(storage_cls)
+
+    def read(self):
+        data = self.storage.read()
+
+        return data
+
+    def write(self, data):
+
+        for table_name in data:
+            table = data[table_name]
+
+            for element_id in table:
+                doc = table[element_id]
+                doc = self._convert(doc)
+
+        self.storage.write(data)
+
+    def _convert(self, doc):
+        """ Recursively convert array and DataFrame to lists/json """
+        for key, value in doc.items():
+            if isinstance(value, (opt.pandas.Series, opt.pandas.DataFrame, opt.pandas.Panel)):
+                doc[key] = value.to_json()
+            elif isinstance(value, opt.np.ndarray):
+                doc[key] = value.tolist()
+            elif isinstance(value, dt.datetime):
+                doc[key] = value.isoformat()
+            elif isinstance(value, dict):
+                # Make sure we recurse into sub-docs
+                doc[key] = self._convert(value)
+        return doc
+
+    def close(self):
+        self.storage.close()
---
+++ tests/test_observers/test_tinydb_observer.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import division, print_function, unicode_literals
+
+import datetime
+
+import mock
+# import mongomock
+import pytest
+import tempfile
+
+from tinydb import Query
+
+from sacred.dependencies import get_digest
+from sacred.observers.tinydb import TinyDbObserver
+from sacred import optional as opt
+
+T1 = datetime.datetime(1999, 5, 4, 3, 2, 1, 0)
+T2 = datetime.datetime(1999, 5, 5, 5, 5, 5, 5)
+
+
+@pytest.fixture()
+def tinydb_obs(tmpdir):
+    return TinyDbObserver.create(path=tmpdir.strpath, name='testdb')
+
+
+@pytest.fixture()
+def sample_run():
+    exp = {'name': 'test_exp', 'sources': [], 'doc': '', 'base_dir': '/tmp'}
+    host = {'hostname': 'test_host', 'cpu_count': 1, 'python_version': '3.4'}
+    config = {'config': 'True', 'foo': 'bar', 'answer': 42}
+    command = 'run'
+    meta_info = {'comment': 'test run'}
+    return {
+        '_id': 'FEDCBA9876543210',
+        'ex_info': exp,
+        'command': command,
+        'host_info': host,
+        'start_time': T1,
+        'config': config,
+        'meta_info': meta_info,
+    }
+
+
+# def test_tindb_observer_started_event_creates_run(tinydb_obs, sample_run):
+#     sample_run['_id'] = None
+#     _id = tinydb_obs.started_event(**sample_run)
+#     assert _id is not None
+#     assert len(tinydb_obs.runs) == 1
+#     db_run = tinydb_obs.runs.get(eid=1)
+#     assert db_run == {
+#         '_id': _id,
+#         'experiment': sample_run['ex_info'],
+#         'format': tinydb_obs.VERSION,
+#         'command': sample_run['command'],
+#         'host': sample_run['host_info'],
+#         'start_time': sample_run['start_time'],
+#         'heartbeat': None,
+#         'info': {},
+#         'captured_out': '',
+#         'artifacts': [],
+#         'config': sample_run['config'],
+#         'meta': sample_run['meta_info'],
+#         'status': 'RUNNING',
+#         'resources': []
+#     }
+
+
+def test_tinydb_observer_started_event_uses_given_id(tinydb_obs, sample_run):
+    _id = tinydb_obs.started_event(**sample_run)
+    assert _id == sample_run['_id']
+    assert len(tinydb_obs.runs) == 1
+    db_run = tinydb_obs.runs.get(eid=1)
+    assert db_run['_id'] == sample_run['_id']
+
+
+# # Got to here !!!!
+# def test_mongo_observer_equality(mongo_obs):
+#     runs = mongo_obs.runs
+#     fs = mock.MagicMock()
+#     m = TinyDbObserver(runs, fs)
+#     assert mongo_obs == m
+#     assert not mongo_obs != m
+
+#     assert not mongo_obs == 'foo'
+#     assert mongo_obs != 'foo'
+
+
+# def test_mongo_observer_heartbeat_event_updates_run(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     info = {'my_info': [1, 2, 3], 'nr': 7}
+#     outp = 'some output'
+#     with tempfile.NamedTemporaryFile() as f:
+#         f.write(outp.encode())
+#         f.flush()
+#         mongo_obs.heartbeat_event(info=info, cout_filename=f.name,
+#                                   beat_time=T2)
+
+#     assert mongo_obs.runs.count() == 1
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['heartbeat'] == T2
+#     assert db_run['info'] == info
+#     assert db_run['captured_out'] == outp
+
+
+# def test_mongo_observer_completed_event_updates_run(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     mongo_obs.completed_event(stop_time=T2, result=42)
+
+#     assert mongo_obs.runs.count() == 1
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['stop_time'] == T2
+#     assert db_run['result'] == 42
+#     assert db_run['status'] == 'COMPLETED'
+
+
+# def test_mongo_observer_interrupted_event_updates_run(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     mongo_obs.interrupted_event(interrupt_time=T2, status='INTERRUPTED')
+
+#     assert mongo_obs.runs.count() == 1
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['stop_time'] == T2
+#     assert db_run['status'] == 'INTERRUPTED'
+
+
+# def test_mongo_observer_failed_event_updates_run(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     fail_trace = "lots of errors and\nso\non..."
+#     mongo_obs.failed_event(fail_time=T2,
+#                            fail_trace=fail_trace)
+
+#     assert mongo_obs.runs.count() == 1
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['stop_time'] == T2
+#     assert db_run['status'] == 'FAILED'
+#     assert db_run['fail_trace'] == fail_trace
+
+
+# def test_mongo_observer_artifact_event(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     filename = "setup.py"
+#     name = 'mysetup'
+
+#     mongo_obs.artifact_event(name, filename)
+
+#     assert mongo_obs.fs.put.called
+#     assert mongo_obs.fs.put.call_args[1]['filename'].endswith(name)
+
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['artifacts']
+
+
+# def test_mongo_observer_resource_event(mongo_obs, sample_run):
+#     mongo_obs.started_event(**sample_run)
+
+#     filename = "setup.py"
+#     md5 = get_digest(filename)
+
+#     mongo_obs.resource_event(filename)
+
+#     assert mongo_obs.fs.exists.called
+#     mongo_obs.fs.exists.assert_any_call(filename=filename)
+
+#     db_run = mongo_obs.runs.find_one()
+#     assert db_run['resources'] == [(filename, md5)]
+
+
+# def test_force_bson_encodable_doesnt_change_valid_document():
+#     d = {'int': 1, 'string': 'foo', 'float': 23.87, 'list': ['a', 1, True],
+#          'bool': True, 'cr4zy: _but_ [legal) Key!': '$illegal.key.as.value',
+#          'datetime': datetime.datetime.now(), 'tuple': (1, 2.0, 'three'),
+#          'none': None}
+#     assert force_bson_encodeable(d) == d
+
+
+# def test_force_bson_encodable_substitutes_illegal_value_with_strings():
+#     d = {
+#         'a_module': datetime,
+#         'some_legal_stuff': {'foo': 'bar', 'baz': [1, 23, 4]},
+#         'nested': {
+#             'dict': {
+#                 'with': {
+#                     'illegal_module': mock
+#                 }
+#             }
+#         },
+#         '$illegal': 'because it starts with a $',
+#         'il.legal': 'because it contains a .',
+#         12.7: 'illegal because it is not a string key'
+#     }
+#     expected = {
+#         'a_module': str(datetime),
+#         'some_legal_stuff': {'foo': 'bar', 'baz': [1, 23, 4]},
+#         'nested': {
+#             'dict': {
+#                 'with': {
+#                     'illegal_module': str(mock)
+#                 }
+#             }
+#         },
+#         '@illegal': 'because it starts with a $',
+#         'il,legal': 'because it contains a .',
+#         '12,7': 'illegal because it is not a string key'
+#     }
+#     assert force_bson_encodeable(d) == expected
+
+
+# @pytest.mark.skipif(not opt.has_numpy, reason='needs numpy')
+# def test_numpy_array_to_list_son_manipulator():
+#     from sacred.observers.mongo import NumpyArraysToList
+#     import numpy as np
+#     sonm = NumpyArraysToList()
+#     document = {
+#         'foo': 'bar',
+#         'some_array': np.eye(3),
+#         'nested': {
+#             'ones': np.ones(5)
+#         }
+#     }
+#     mod_doc = sonm.transform_incoming(document, 'fake_collection')
+#     assert mod_doc['foo'] == 'bar'
+#     assert mod_doc['some_array'] == [[1.0, 0.0, 0.0],
+#                                      [0.0, 1.0, 0.0],
+#                                      [0.0, 0.0, 1.0]]
+#     assert mod_doc['nested']['ones'] == [1.0, 1.0, 1.0, 1.0, 1.0]
+
+
+# @pytest.mark.skipif(not opt.has_pandas, reason='needs pandas')
+# def test_pandas_to_json_son_manipulator():
+#     from sacred.observers.mongo import PandasToJson
+#     import numpy as np
+#     import pandas as pd
+#     sonm = PandasToJson()
+#     document = {
+#         'foo': 'bar',
+#         'some_array': pd.DataFrame(np.eye(3), columns=list('ABC')),
+#         'nested': {
+#             'ones': pd.Series(np.ones(5))
+#         }
+#     }
+#     mod_doc = sonm.transform_incoming(document, 'fake_collection')
+#     assert mod_doc['foo'] == 'bar'
+#     assert mod_doc['some_array'] == {'A': {'0': 1.0, '1': 0.0, '2': 0.0},
+#                                      'B': {'0': 0.0, '1': 1.0, '2': 0.0},
+#                                      'C': {'0': 0.0, '1': 0.0, '2': 1.0}}
+#     assert mod_doc['nested']['ones'] == {"0": 1.0, "1": 1.0, "2": 1.0,
+#                                          "3": 1.0, "4": 1.0}
