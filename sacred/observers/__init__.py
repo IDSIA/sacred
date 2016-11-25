8ecf97b338dd37eaf5a4e2672e33e27cc40d215d
Chris Musselle <chris.j.musselle@gmail.com>
2016-11-25 23:41:32 +0000

    Add TinyDbReader to observers init

--- sacred/observers/__init__.py
+++ sacred/observers/__init__.py
@@ -7,7 +7,7 @@ from sacred.commandline_options import CommandLineOption
 from sacred.observers.base import RunObserver
 from sacred.observers.file_storage import FileStorageObserver
 import sacred.optional as opt
-from sacred.observers.tinydb_hashfs import TinyDbObserver
+from sacred.observers.tinydb_hashfs import TinyDbObserver, TinyDbReader
 
 if opt.has_pymongo:
     from sacred.observers.mongo import MongoObserver
@@ -41,4 +41,4 @@ else:
 
 
 __all__ = ('FileStorageObserver', 'RunObserver', 'MongoObserver',
-           'SqlObserver', 'TinyDbObserver')
+           'SqlObserver', 'TinyDbObserver', 'TinyDbReader')
