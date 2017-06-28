f1791da7309774f6eb227dee964559a8ff6ee8e6
Qwlouse <qwlouse@gmail.com>
2017-06-28 18:22:07 +0200

    Revert "Revert "Feature/tf integration""
    This reverts commit 4e05a17a1e14bea1fbe629e948fb9c014505fbdd.
    
    # Conflicts:
    #	sacred/optional.py

--- sacred/optional.py
+++ sacred/optional.py
@@ -58,6 +58,8 @@ has_tinydb, tinydb = optional_import('tinydb', 'tinydb_serialization',
                                      'hashfs')
 has_requests, requests = optional_import('requests')
 has_telegram, telegram = optional_import('telegram')
+has_tensorflow, tensorflow = optional_import("tensorflow")
+
 
 # Python 2 and 3 compatible basestring
 if sys.version_info[0] == 3:
---
+++ tests/test_stflow/test_internal.py
@@ -0,0 +1,45 @@
+from sacred.stflow.internal import ContextMethodDecorator
+
+
+def test_context_method_decorator():
+    " Ensure that ContextMethodDecorator can intercept method calls "
+    class FooClass():
+        def __init__(self, x):
+            self.x = x
+
+        def do_foo(self, y, z):
+            print("foo")
+            print(y)
+            print(z)
+            return y * self.x + z
+
+    def decorate_three_times(instance, original_method, original_args,
+                             original_kwargs):
+        print("three_times")
+        print(original_args)
+        print(original_kwargs)
+        return original_method(instance, *original_args, **original_kwargs) * 3
+
+    with ContextMethodDecorator(FooClass, "do_foo", decorate_three_times):
+        foo = FooClass(10)
+        assert foo.do_foo(5, 6) == (5 * 10 + 6) * 3
+        assert foo.do_foo(5, z=6) == (5 * 10 + 6) * 3
+        assert foo.do_foo(y=5, z=6) == (5 * 10 + 6) * 3
+    assert foo.do_foo(5, 6) == (5 * 10 + 6)
+    assert foo.do_foo(5, z=6) == (5 * 10 + 6)
+    assert foo.do_foo(y=5, z=6) == (5 * 10 + 6)
+
+    def decorate_three_times_with_exception(instance, original_method,
+                                            original_args, original_kwargs):
+        raise RuntimeError("This should be caught")
+
+    exception = False
+    try:
+        with ContextMethodDecorator(FooClass, "do_foo",
+                                    decorate_three_times_with_exception):
+            foo = FooClass(10)
+            this_should_raise_exception = foo.do_foo(5, 6)
+    except RuntimeError:
+        exception = True
+    assert foo.do_foo(5, 6) == (5 * 10 + 6)
+    assert exception is True
