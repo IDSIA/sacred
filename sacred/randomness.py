5e45928a2e998267454b8b55e3297a91199f3e90
Qwlouse <qwlouse@gmail.com>
2017-07-06 16:43:45 +0200

    also set the tensorflow seed if it is imported
    fixes #178

--- sacred/randomness.py
+++ sacred/randomness.py
@@ -3,6 +3,7 @@
 from __future__ import division, print_function, unicode_literals
 
 import random
+import sys
 import sacred.optional as opt
 
 __sacred__ = True  # marks files that should be filtered from stack traces
@@ -29,3 +30,6 @@ def set_global_seed(seed):
     random.seed(seed)
     if opt.has_numpy:
         opt.np.random.seed(seed)
+    if 'tensorflow' in sys.modules:
+        import tensorflow as tf
+        tf.set_random_seed(seed)
