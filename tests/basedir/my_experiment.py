0cd42478586c9cb1febb6b76eea068aac8869a1e
Aiden Nibali <dismaldenizen@gmail.com>
2018-01-25 09:08:01 +1100

    Support custom experiment base directory

---
+++ tests/basedir/my_experiment.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+A file for testing the gathering of sources when a custom base directory is set.
+"""
+
+from tests.foo import bar
+
+
+def some_func():
+    pass
