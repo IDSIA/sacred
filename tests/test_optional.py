419c4d38e5d35f6e59b805deec062e5fa52b8154
JarnoRFB <ruedigerbusche@web.de>
2019-07-30 20:28:18 +0200

    Beautify

--- tests/test_optional.py
+++ tests/test_optional.py
@@ -6,13 +6,13 @@ from sacred.optional import optional_import, get_tensorflow, modules_exist
 
 
 def test_optional_import():
-    has_pytest, pyt = optional_import('pytest')
+    has_pytest, pyt = optional_import("pytest")
     assert has_pytest
     assert pyt == pytest
 
 
 def test_optional_import_nonexisting():
-    has_nonex, nonex = optional_import('clearlynonexistingpackage')
+    has_nonex, nonex = optional_import("clearlynonexistingpackage")
     assert not has_nonex
     assert nonex is None
 
@@ -21,6 +21,7 @@ def test_get_tensorflow():
     """Test that get_tensorflow() runs without error."""
     pytest.importorskip("tensorflow")
     get_tensorflow()
+
 
 def test_module_exists_for_tensorflow():
     """Check that module_exist returns true if tf is there."""
