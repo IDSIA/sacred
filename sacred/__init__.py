979075e8a79b502a20f4bef41b696575b7263fa9
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-08-02 17:45:44 +0200

    Made the dictionary intern to the function.

--- sacred/__init__.py
+++ sacred/__init__.py
@@ -11,9 +11,8 @@ from sacred.settings import SETTINGS
 from sacred.experiment import Experiment
 from sacred.ingredient import Ingredient
 from sacred import observers
-from sacred.host_info import host_info_getter
 
 
-__all__ = ('Experiment', 'Ingredient', 'observers', 'host_info_getter',
+__all__ = ('Experiment', 'Ingredient', 'observers',
            '__version__', '__author__', '__author_email__', '__url__',
            'SETTINGS')
