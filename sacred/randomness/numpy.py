0db19c2ca92754fcc5cdc7d92c8c44c4c2c0f3a3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-08-30 18:33:21 +0200

    Moved numpy.

--- sacred/randomness/numpy.py
+++ sacred/randomness/numpy.py
@@ -1,16 +1,21 @@
-import numpy as np
 from .save_state import SaveState
 
 
 def set_numpy_seed(seed):
+    import numpy as np
+
     np.random.seed(seed)
 
 
 def get_state():
+    import numpy as np
+
     return np.random.get_state()
 
 
 def set_state(state):
+    import numpy as np
+
     np.random.set_state(state)
 
 
