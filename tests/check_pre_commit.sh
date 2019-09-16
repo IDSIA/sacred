1d5106f6e46d19ba7872e2f5a6f8cf030053f60e
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-09-16 12:34:02 +0200

    Useless line.

--- tests/check_pre_commit.sh
+++ tests/check_pre_commit.sh
@@ -5,6 +5,3 @@ set -e
 pip install -e .
 pip install pre-commit
 pre-commit run --all-files
-
-# check that no files have changed.
-git diff-tree --no-commit-id --name-only -r $REVISION | xargs pre-commit run --files
