f69d3eaeb36c9b6e0c6aad339e5db78e3e10a5a3
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-09-19 15:00:45 +0000

    Forgot to add it to the list of default options.

--- sacred/experiment.py
+++ sacred/experiment.py
@@ -596,6 +596,7 @@ DEFAULT_COMMAND_LINE_OPTIONS = [
     commandline_options.capture_option,
     commandline_options.print_config_option,
     commandline_options.name_option,
+    commandline_options.priority_option,
     commandline_options.unobserved_option,
     commandline_options.beat_interval_option,
     commandline_options.queue_option,
