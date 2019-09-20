a6018b222d5ff0767b27abdd50ce287335ba53a8
davebulaval <david.beauchemin.5@ulaval.ca>
2019-09-20 17:22:26 -0400

    added example on how to use a notificator

---
+++ examples/notification.py
@@ -0,0 +1,17 @@
+from notif.notificator import SlackNotificator
+
+import sacred
+
+ex = sacred.Experiment('deep-address-tagger')
+
+notificator = SlackNotificator(
+    webhook_url="webhook_url_to_throw_notification")
+# Change for a valid webhook url. Read the doc about Slack webhook for information on how to get one.
+
+ex.add_notificator(notificator=notificator)
+
+
+@ex.automain
+def main():
+    print('3')
+    # This will also throw a message into your Slack channel.
