dda15ecdfee807b183dfe206503444c831abbc2e
davebulaval <david.beauchemin.5@ulaval.ca>
2019-09-22 19:18:21 -0400

    found error for examples/notification.py

--- examples/notification.py
+++ examples/notification.py
@@ -2,10 +2,9 @@ from notif.notificator import SlackNotificator
 
 import sacred
 
-ex = sacred.Experiment('deep-address-tagger')
+ex = sacred.Experiment("deep-address-tagger")
 
-notificator = SlackNotificator(
-    webhook_url="webhook_url_to_throw_notification")
+notificator = SlackNotificator(webhook_url="webhook_url_to_throw_notification")
 # Change for a valid webhook url. Read the doc about Slack webhook for information on how to get one.
 
 ex.add_notificator(notificator=notificator)
@@ -13,5 +12,5 @@ ex.add_notificator(notificator=notificator)
 
 @ex.automain
 def main():
-    print('3')
+    print("3")
     # This will also throw a message into your Slack channel.
