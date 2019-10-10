950d9468c2298a7bddef04b3e191e20054df5f04
Rüdiger Busche <ruedigerbusche@web.de>
2019-10-10 17:46:27 +0200

    Add documentation for queue observer (#677)
    * Add documentaton for queue observer
    
    * Add class docstring
    
    * Fix pymongo import
    
    * Add usecase
    
    * Specify a bit more

--- sacred/observers/__init__.py
+++ sacred/observers/__init__.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python
-# coding=utf-8
-
 from sacred.observers.base import RunObserver
 from sacred.observers.file_storage import FileStorageObserver
-from sacred.observers.mongo import MongoObserver
+from sacred.observers.mongo import MongoObserver, QueuedMongoObserver
 from sacred.observers.sql import SqlObserver
 from sacred.observers.tinydb_hashfs import TinyDbObserver, TinyDbReader
 from sacred.observers.slack import SlackObserver
 from sacred.observers.telegram_obs import TelegramObserver
 from sacred.observers.s3_observer import S3Observer
+from sacred.observers.queue import QueueObserver
 
 
 __all__ = (
     "FileStorageObserver",
     "RunObserver",
     "MongoObserver",
+    "QueuedMongoObserver",
     "SqlObserver",
     "TinyDbObserver",
     "TinyDbReader",
     "SlackObserver",
     "TelegramObserver",
     "S3Observer",
+    "QueueObserver",
 )
