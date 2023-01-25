57e2b1087f1596c1a2aaee4cf67488a97b77a605
Thilo von Neumann <tvn@mail.upb.de>
2023-01-25 09:57:15 +0100

    Allow *.ipynb files as source files

--- sacred/dependencies.py
+++ sacred/dependencies.py
@@ -377,7 +377,7 @@ $
 
 def get_py_file_if_possible(pyc_name):
     """Try to retrieve a X.py file for a given X.py[c] file."""
-    if pyc_name.endswith((".py", ".so", ".pyd")):
+    if pyc_name.endswith((".py", ".so", ".pyd", ".ipynb")):
         return pyc_name
     assert pyc_name.endswith(".pyc")
     non_compiled_file = pyc_name[:-1]
